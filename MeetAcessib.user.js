a367773bb0efd0289e505eb4c6361ca4d3fab0e9
jeimison3 <jeimison.ti@gmail.com>
2021-07-16 11:39:53 -0300

    Versão concluída

--- MeetAcessib.user.js
+++ MeetAcessib.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MeetAcessib
 // @namespace    https://github.com/jeimison3/MeetAcessib
-// @version      0.1.0
+// @version      0.1.1b
 // @description  Ferramentas assistivas para Google Meet
 // @author       jeimison3
 // @match        http://meet.google.com/*
